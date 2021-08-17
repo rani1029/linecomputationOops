cdcbcdf7bf54a1e559a9d46189cd4101b701afad
rani1029 <ranig1029@gmail.com>
2021-08-18 01:09:00 +0530

    Use case 2  added

--- LinecomputationOops/Program.cs
+++ LinecomputationOops/Program.cs
@@ -8,9 +8,12 @@ namespace LinecomputationOops
         {
             Console.WriteLine("Welcomr to line computation using oops concept");
             Program ln = new Program();
+            Program ln2 = new Program();
             double ll = ln.Length(4, 5, 5, 9);
-
+            double ll2 = ln.Length(4, 8, 5, 9);
             Console.WriteLine("Length of the line" + " " + ll);
+            Console.WriteLine("Length of the line 2 is  " + " " + ll2);
+            Console.WriteLine("is lenth of two lines equal " + ll.Equals(ll2));
 
         }
         public double Length(int x1, int x2, int y1, int y2)
