9a1e673ff7617136db62bf47841c9cca91bb8545
rani1029 <ranig1029@gmail.com>
2021-08-18 00:55:19 +0530

    welcome msg added

---
+++ LinecomputationOops/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LinecomputationOops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcomr to line computation using oops concept");
+        }
+    }
+}
